661c1a07af9a80d3b928f36295aa8524b7ebedd1
RNCstar <r.n.c.star2002@gmail.com>
2023-11-18 04:56:12 -0800

    BOM

---
+++ BOM.js
@@ -0,0 +1,16 @@
+// window.innerHeight - the inner height of the browser window (in pixels)
+// window.innerWidth - the inner width of the browser window (in pixels)
+// window.open() - open a new window
+// window.close() - close the current window
+// window.moveTo() - move the current window
+// window.resizeTo() - resize the current window
+// and we also see scren obj in js like window
+
+// History
+
+// history.back() - same as clicking back in the browser
+// history.forward() - same as clicking forward in the browser
+
+alert("hi");
+confirm("it's OK?");
+prompt("user:");
